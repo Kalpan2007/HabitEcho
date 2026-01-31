08825fe942406f931af6eb114e624385969aa151
Kalpan2007 <kalpankaneriya@gmail.com>
2026-01-31 11:33:50 +0530

    SetUp React-Native-Expo App For HabitEcho

---
+++ Habitechoapp/src/screens/HomeScreen.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export default function HomeScreen() {
+    return (
+        <SafeAreaView className="flex-1 items-center justify-center bg-white">
+            <Text className="text-xl font-bold text-gray-800">
+                HabitEcho Mobile App
+            </Text>
+        </SafeAreaView>
+    );
+}
